700511480ffa7e0df02d81a8464ee503ba326558
Robert McCreary Mannino <fooyinfighter@gmail.com>
2024-01-18 15:22:46 -0800

    rotate images

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function handleClick() {
 }
 
 picture1.onclick = handleClick;
+
+let indexOfCurrentPic = 0;
+let pics = ["picture1.jpg", "picture2.jpg", "picture3.jpg", "picture4.jpg"]
+function changeImage() {
+  let i = indexOfCurrentPic + 1;
+  picture1.src = pics[i];
+  if(i<3) {
+    indexOfCurrentPic++;
+  } else {
+    indexOfCurrentPic = 0;
+  }
+}
+nextImage.onclick = changeImage;
